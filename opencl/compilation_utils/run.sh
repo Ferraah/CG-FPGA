a103e1768ce920c4da8d6904b33b7c3b1d0a3e6f
MeluXina user <u101373@mel3015-ib0.meluxina.lxp.lu>
2024-07-19 17:37:16 +0200

    Scalability results.

--- opencl/compilation_utils/run.sh
+++ opencl/compilation_utils/run.sh
@@ -3,10 +3,10 @@
 #SBATCH --account p200301
 #SBATCH --partition fpga
 #SBATCH --qos default
-#SBATCH --nodes 1
+#SBATCH --nodes 8
 #SBATCH --time 01:00:00
-#SBATCH --output run_5k_1f.out
-#SBATCH --error run_5k_1f.out
+#SBATCH --output run_5k_16f.out
+#SBATCH --error run_5k_16f.out
 
 #Load software environment
 module_load_fpga
@@ -14,4 +14,4 @@ module_load_fpga
 cd /home/users/u101373/CG-FPGA/opencl/
 # Create folders
 
-make -j && srun -n 1 ./cg_opencl
+make -j && srun -n 16 ./cg_opencl
